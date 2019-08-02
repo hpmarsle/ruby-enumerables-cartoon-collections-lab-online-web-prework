43cae3477838bc7d481d905eff5560087cc26b0f
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-08-02 03:13:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves (dwarves)
 end
 
 def summon_captain_planet (calls)
-  calls.collect{|call| call.capitalize + !}
+  calls.collect{|call| call.capitalize + !Z}
 end
 
 def long_planeteer_calls# code an argument here
