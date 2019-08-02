e01031b804c76cd7eaf3aa54a809ceff0154a9d9
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-08-02 03:18:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet (calls)
   calls.collect{|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  #check each item in array to see if more than 4 characters (length)
 end
 
 def find_the_cheese# code an argument here
